55cb8fe9286453fe322883a0b3fae221d6755fb3
KEERTHIG DEVI <sdkeerthigadevi@gmail.com>
2025-08-16 17:39:15 +0530

    Created custom react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>
+      Hello world | created two project folder structure using react and vite, react one is running by npm run start and vite is by npm run dev and the react one is not a prefered method as it takes more time and some other issues where as vite is a faster method.
+    </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from './Chai'
+function App() {
+  
+  const username = "cahi aur code"
+
+  return (
+    <>
+      <Chai/>
+      <h1>Hello {username}</h1> 
+      {/* evaluated expression */}
+      <p>have a nice day</p>
+    </>
+  )
+}
+
+export default App
